3440396fb5a7cb764f410825a4417fae002327db
Virendra-Khorwal <virendrakhorwalvk@gmail.com>
2022-07-17 22:22:48 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const Port = process.env.Port
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.json({message:'Welcome to Support Desk API'})
+})
+
+// ROutes
+app.use('/api/users', require('./routes/userRoutes') )
+
+app.listen(Port, ()=> console.log(`Server started at port number ${Port}`))

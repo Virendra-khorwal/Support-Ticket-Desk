ae786ccc13cdd9483bd8bd512f2c6911b26c7e6c
Virendra-Khorwal <virendrakhorwalvk@gmail.com>
2022-07-18 20:10:39 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
+
 const express = require('express')
+const colors = require('colors')
+const { errorHandler } = require('./middleware/errorMiddleWare')
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 
-const Port = process.env.Port
+const Port = process.env.Port || 5000
+
+// connect to database
+connectDB()
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
 
 app.get('/', (req,res) => {
     res.json({message:'Welcome to Support Desk API'})
 })
 
-// ROutes
+// Routes
 app.use('/api/users', require('./routes/userRoutes') )
+
+app.use(errorHandler)
 
 app.listen(Port, ()=> console.log(`Server started at port number ${Port}`))

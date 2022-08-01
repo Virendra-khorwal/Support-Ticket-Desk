477f28971665da5019e36621fc0d97af9f3bb099
Virendra-Khorwal <virendrakhorwalvk@gmail.com>
2022-08-01 18:23:14 +0530

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleWare')
@@ -15,13 +15,23 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
-app.get('/', (req,res) => {
-    res.json({message:'Welcome to Support Desk API'})
-})
+
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production') {
+    //set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build','index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.json({ message: 'Welcome to Support Desk API' })
+    })
+}
 
 app.use(errorHandler)
 
